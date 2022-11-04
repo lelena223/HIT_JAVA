dea511ad323ab22bfdae8eb509876ebb2f0dee2a
Lena192837465 <lelenahkht2003@gmail.com>
2022-11-04 17:52:58 +0700

    BaiTap

--- src/Buoi2/Bai4.java
+++ src/Buoi2/Bai4.java
@@ -3,54 +3,70 @@ package Buoi2;
 import java.util.Scanner;
 
 public class Bai4 {
-	public static int Check(int x)
+	public static boolean Check(int x)
 	{
 		if(x<2)
 		{
-			return 0;
+			return false;
 		}
-		for(int i=2;i<Math.sqrt(x);i++)
+		else 
 		{
-			if(x % i == 0)
+			for(int i=2;i<Math.sqrt(x);i++)
 			{
-				return 0;
-			}
-		}
-		return 1;
-	}
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		System.out.print("Nhap n: ");
-		int n = sc.nextInt();
-		int[] b =new int[100];
-		int[] a = new int[n];
-		for(int i=0;i<a.length;i++)
-		{
-			a[i] = sc.nextInt();
-		}	
-		int dem=0;
-		for(int i=0;i<a.length;i++)
-		{
-			if(a[i]>10 && Check(a[i])==1)
-			{
-				while(a[i]%10 != 0)
+				if(x % i == 0)
 				{
-					b[i]=n%10;
-					n=n/10;
-					dem++;
+					return false;
 				}
 			}
 		}
-		if(dem==0)
+		
+		return true;
+	}
+	public static boolean SNT(int x)
+	{
+		int dem=0;
+		int i=0;
+				while(x>0) 
+				{
+					int y = x%10;
+					if(Check(y)==true)
+					{
+						i++;
+					}
+					x=x/10;
+					dem++;
+				}
+			if(i==dem) 
+			{
+				return true;
+			}
+			else 
+			{
+				return false;
+			}
+	}
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Nhap n: ");
+		int n = sc.nextInt();
+		int[] a = new int[n];
+		for(int i=0;i<n;i++)
+		{
+			a[i] = sc.nextInt();
+		}
+		int dem=0;
+		for(int i=0; i<n; i++) 
+		{
+			if(SNT(a[i])==true && Check(a[i])==true && a[i]>10) 
+			{
+				System.out.print(a[i] + " ");
+				dem++;
+			}
+		}
+		if(dem==0) 
 		{
 			System.out.print("NO");
-		}
-		else
-		{
-			for(int i=0;i<100;i++)
-			{
-				System.out.print(b[i] + " ");
-			}
 		}
 	}
 }
